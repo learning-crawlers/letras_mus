34daa3d41fa8b000d5db10a9f44f594678d06181
Antonio Neto <antoniomrneto@gmail.com>
2016-03-15 16:49:19 -0300

    Primeira versão.

---
+++ letras_mus.py
@@ -0,0 +1,40 @@
+#-*- coding: utf-8 -*-
+#!/usr/bin/env python
+import requests
+from sys import argv, stdout
+from bs4 import BeautifulSoup
+import locale
+import sys
+
+
+def get_mus_list(artist_url):
+    r = requests.get("https://www.letras.mus.br/%s" % artist_url)
+    
+    soup = BeautifulSoup(r.text, 'html.parser')
+
+    return [(track.text, 
+             track.select("a")[0].attrs["href"])  
+                for track in soup.findAll("li", {"itemprop":"tracks"})]
+
+def get_lyrics(mus_url):
+    r = requests.get("https://www.letras.mus.br%s" % mus_url)
+    soup = BeautifulSoup(r.text, 'html.parser')    
+    lyrics = soup.find("div", {"class": "cnt-letra"})
+    
+    return u"\n".join([p.text for p in lyrics.findAll("p")])
+    
+    
+if __name__ == "__main__":
+    reload(sys)
+    sys.setdefaultencoding('utf-8')
+    for mus in get_mus_list(argv[1]):
+        print mus[0].upper()
+        print get_lyrics(mus[1])
+        print "==============================================================================================="
+        print
+
+
+
+      
+
+
