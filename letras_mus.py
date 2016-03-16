28b01274b95f99f47b563bd8698abb41e525edcb
Antonio Neto <antoniomrneto@gmail.com>
2016-03-16 15:55:15 -0300

    Add usage and example. Fix line breaks on paragraphs. Imports refactoring.

--- letras_mus.py
+++ letras_mus.py
@@ -2,9 +2,7 @@
 #-*- coding: utf-8 -*-
 
 import requests
-from sys import argv
 from bs4 import BeautifulSoup
-import sys
 
 
 def get_track_list(artist_url):
@@ -18,8 +16,11 @@ def get_lyrics(mus_url):
     r = requests.get("https://www.letras.mus.br%s" % mus_url)
     soup = BeautifulSoup(r.text, 'html.parser')    
     lyrics = soup.find("div", {"class": "cnt-letra"})
-    
-    return u"\n".join([p.text for p in lyrics.findAll("p")])
+
+    for b in lyrics.findAll("br"):
+        b.replaceWith("\n")
+
+    return u"\n\n".join([p.text for p in lyrics.findAll("p")])
     
 def get_lyrics_list(url_list, pool_size=8):
     from multiprocessing import Pool
@@ -28,16 +29,23 @@ def get_lyrics_list(url_list, pool_size=8):
     return pool.map(get_lyrics, url_list)
 
 def pretty_print(mus_name, lyrics):
-    import re
     print mus_name.upper()
-    print re.sub(r"[A-Z]", lambda m: m.expand("\n\g<0>"), lyric)
+    print lyric 
+    print 
     print "="*80
     print
 
 if __name__ == "__main__":
+    import sys
+    
+    if len(sys.argv) != 2:
+        sys.stderr.write("usage:\n\t%s <music-url>\n" % sys.argv[0])
+        sys.stderr.write("example:\n\t%s mc-anitta\n" % sys.argv[0])
+        sys.exit(1)
+        
     reload(sys)
     sys.setdefaultencoding('utf-8')
-    track_list = get_track_list(argv[1])
+    track_list = get_track_list(sys.argv[1])
     lyrics = get_lyrics_list([track["url"] for track in track_list])
     for i, lyric in enumerate(lyrics):
         pretty_print(track_list[i]["name"], lyric)
